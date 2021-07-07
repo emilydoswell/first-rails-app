e30f42d10df61bc626527bfc2920dbfc8cd7a8f3
emily doswell <emilydoswell@yahoo.co.uk>
2021-07-07 12:55:29 +0100

    Added homepage root to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #verb         to: controller#action
+  # Home page, no path.
+  root to: 'pages#home'
+  # verb         to: controller#action
   get '/about', to: "pages#about"
   get '/contact', to: "pages#contact"
 end
